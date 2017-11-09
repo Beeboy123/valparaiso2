ab1115fc3cae8dcd3fa953ad492ee30a51edaef4
Jeremy Nathaniel <jeremy.ntl@gmail.com>
2017-11-09 16:55:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+valparaiso
+==========
+
+A Symfony project created on November 9, 2017, 2:45 pm.
---
+++ src/Utilisateurs/UtilisateursBundle/Entity/Utilisateur.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerem
+ * Date: 09/11/2017
+ * Time: 16:01
+ */
+
+
+namespace Utilisateurs\UtilisateursBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
